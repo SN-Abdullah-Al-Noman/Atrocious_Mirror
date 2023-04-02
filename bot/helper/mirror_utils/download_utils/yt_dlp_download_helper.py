c1a557bbf537c6d812e99842871a7ed083c0f41a
AtrociousMirrorBot <101085500+AtrociousMirrorBot@users.noreply.github.com>
2023-04-02 17:20:14 +0600

    Update yt_dlp_download_helper.py

--- bot/helper/mirror_utils/download_utils/yt_dlp_download_helper.py
+++ bot/helper/mirror_utils/download_utils/yt_dlp_download_helper.py
@@ -270,16 +270,16 @@ class YoutubeDLHelper:
         DAILY_MIRROR_LIMIT = config_dict['DAILY_MIRROR_LIMIT'] * 1024**3 if config_dict['DAILY_MIRROR_LIMIT'] else config_dict['DAILY_MIRROR_LIMIT']
         DAILY_LEECH_LIMIT = config_dict['DAILY_LEECH_LIMIT'] * 1024**3 if config_dict['DAILY_LEECH_LIMIT'] else config_dict['DAILY_LEECH_LIMIT']
         if DAILY_MIRROR_LIMIT and not self.listener.isLeech and user_id != OWNER_ID and not is_sudo(user_id) and not is_paid(user_id) and (self.__size >= (DAILY_MIRROR_LIMIT - getdailytasks(user_id, check_mirror=True)) or DAILY_MIRROR_LIMIT <= getdailytasks(user_id, check_mirror=True)):
-            mssg = f"Daily Mirror Limit is {get_readable_file_size(DAILY_MIRROR_LIMIT)}\nYou have exhausted Today's Mirror Limit or Size of your Mirror is greater than free Limits.\n#TRY_AGAIN_TOMORROW #Daily_Mirror_Limit"
+            mssg = f"Daily Mirror Limit is {get_readable_file_size(DAILY_MIRROR_LIMIT)}\nYou have exhausted Today's Mirror Limit or Size of your Mirror is greater than free Limits.\nTry again tomorrow"
             if config_dict['PAID_SERVICE'] is True:
-                mssg += f'\n#Buy Paid Service'
+                mssg += f'\nBuy Paid Service'
             return self.__onDownloadError(mssg)
         elif not self.listener.isLeech: msize = getdailytasks(user_id, upmirror=self.__size, check_mirror=True); LOGGER.info(f"User : {user_id} | Daily Mirror Size : {get_readable_file_size(msize)}")
 
         if DAILY_LEECH_LIMIT and self.listener.isLeech and user_id != OWNER_ID and not is_sudo(user_id) and not is_paid(user_id) and (self.__size >= (DAILY_LEECH_LIMIT - getdailytasks(user_id, check_leech=True)) or DAILY_LEECH_LIMIT <= getdailytasks(user_id, check_leech=True)):
-            mssg = f"Daily Leech Limit is {get_readable_file_size(DAILY_LEECH_LIMIT)}\nYou have exhausted Today's Leech Limit or Size of your Leech is greater than free Limits.\n#TRY_AGAIN_TOMORROW #Daily_Leech_Limit"
+            mssg = f"Daily Leech Limit is {get_readable_file_size(DAILY_LEECH_LIMIT)}\nYou have exhausted Today's Leech Limit or Size of your Leech is greater than free Limits.\nTry again tomorrow"
             if config_dict['PAID_SERVICE'] is True:
-                mssg += f'\n#Buy Paid Service'
+                mssg += f'\nBuy Paid Service'
             return self.__onDownloadError(mssg)
         elif self.listener.isLeech: lsize = getdailytasks(user_id, upleech=self.__size, check_leech=True); LOGGER.info(f"User : {user_id} | Daily Leech Size : {get_readable_file_size(lsize)}")
 
