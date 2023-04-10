1f665dddf3ef60df060999ba34b54a5d2cc3cb1b
Abdullah Al Noman <101085500+AtrociousMirrorBot@users.noreply.github.com>
2023-04-10 11:39:09 +0600

    Update bot_settings.py

--- bot/modules/bot_settings.py
+++ bot/modules/bot_settings.py
@@ -1054,9 +1054,9 @@ def update_private_file(update, context, omsg):
         if fn == 'accounts':
             if ospath.exists('accounts'):
                 srun(["rm", "-rf", "accounts"])
-            config_dict['USE_SERVICE_ACCOUNTS'] = True
+            config_dict['USE_SERVICE_ACCOUNTS'] = False
             if DATABASE_URL:
-                DbManger().update_config({'USE_SERVICE_ACCOUNTS': True})
+                DbManger().update_config({'USE_SERVICE_ACCOUNTS': False})
         elif file_name in ['.netrc', 'netrc']:
             srun(["touch", ".netrc"])
             srun(["cp", ".netrc", "/root/.netrc"])
