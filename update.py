7f574ec6f6260733f5f0256171f17956c2bca1e1
Abdullah Al Noman <101085500+AtrociousMirrorBot@users.noreply.github.com>
2023-05-15 10:15:48 +0600

    Add files via upload

--- update.py
+++ update.py
@@ -43,11 +43,6 @@ if DATABASE_URL is not None:
         environ['UPDATE_PACKAGES'] = config_dict.get('UPDATE_PACKAGES', 'False')
     conn.close()
 
-UPDATE_PACKAGES = environ.get('UPDATE_PACKAGES', 'False')
-if UPDATE_PACKAGES.lower() == 'true':
-    packages = [dist.project_name for dist in working_set]
-    scall("pip install " + ' '.join(packages), shell=True)
-
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
 if len(UPSTREAM_REPO) == 0:
    UPSTREAM_REPO = None
