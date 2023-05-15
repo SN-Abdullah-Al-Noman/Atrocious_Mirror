88ce06ffedfe5847401c6fb5cf6800af32102a85
Abdullah Al Noman <101085500+AtrociousMirrorBot@users.noreply.github.com>
2023-05-15 22:53:10 +0600

    Update broadcast.py

--- bot/modules/broadcast.py
+++ bot/modules/broadcast.py
@@ -5,7 +5,6 @@ from bot import bot, dispatcher, LOGGER, config_dict
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.message_utils import sendMessage
 
-
 def broadcast(update, context):
     reply_to = update.message.reply_to_message
 
@@ -18,16 +17,14 @@ def broadcast(update, context):
         users_count = db.users.count_documents({})
     
         chat_ids = [str(user["_id"]) for user in users_collection.find({}, {"_id": 1})]
-
         success = 0
-
+        
         for chat_id in chat_ids:
             try:
                 context.bot.copy_message(chat_id=chat_id, from_chat_id=update.message.chat.id, message_id=reply_to.message_id)
                 success += 1
             except Exception as err:
                 LOGGER.error(err)
-
 
         msg = f"<b>Broadcasting Completed</b>\n"
         msg += f"<b>Total {users_count} users in Database</b>\n"
