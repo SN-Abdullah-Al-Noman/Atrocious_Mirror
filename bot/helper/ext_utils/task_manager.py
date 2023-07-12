e6d01fe818b3303c4f9ce2d15606c9fa6dbc85fc
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2023-07-12 22:29:13 +0600

    Add files via upload

---
+++ bot/helper/ext_utils/task_manager.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python3
+from asyncio import Event
+
+from bot import config_dict, queued_dl, queued_up, non_queued_up, non_queued_dl, queue_dict_lock, LOGGER, download_dict, OWNER_ID, user_data
+from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
+from bot.helper.ext_utils.fs_utils import get_base_name, check_storage_threshold
+from bot.helper.ext_utils.bot_utils import sync_to_async, get_telegraph_list, checking_access, get_readable_file_size, get_user_tasks
+from bot.helper.telegram_helper.message_utils import forcesub, BotPm_check, user_info
+
+async def stop_duplicate_check(name, listener):
+    if (
+        not config_dict['STOP_DUPLICATE']
+        or listener.isLeech
+        or listener.upPath != 'gd'
+        or listener.select
+    ):
+        return False, None
+    LOGGER.info(f'Checking File/Folder if already in Drive: {name}')
+    if listener.compress:
+        name = f"{name}.zip"
+    elif listener.extract:
+        try:
+            name = get_base_name(name)
+        except:
+            name = None
+    if name is not None:
+        telegraph_content, contents_no = await sync_to_async(GoogleDriveHelper().drive_list, name, stopDup=True)
+        if telegraph_content:
+            msg = f"File/Folder is already available in Drive.\nHere are {contents_no} list results:"
+            button = await get_telegraph_list(telegraph_content)
+            return msg, button
+    return False, None
+
+
+async def is_queued(uid):
+    all_limit = config_dict['QUEUE_ALL']
+    dl_limit = config_dict['QUEUE_DOWNLOAD']
+    event = None
+    added_to_queue = False
+    if all_limit or dl_limit:
+        async with queue_dict_lock:
+            dl = len(non_queued_dl)
+            up = len(non_queued_up)
+            if (all_limit and dl + up >= all_limit and (not dl_limit or dl >= dl_limit)) or (dl_limit and dl >= dl_limit):
+                added_to_queue = True
+                event = Event()
+                queued_dl[uid] = event
+    return added_to_queue, event
+
+
+def start_dl_from_queued(uid):
+    queued_dl[uid].set()
+    del queued_dl[uid]
+
+
+def start_up_from_queued(uid):
+    queued_up[uid].set()
+    del queued_up[uid]
+
+
+async def start_from_queued():
+    if all_limit := config_dict['QUEUE_ALL']:
+        dl_limit = config_dict['QUEUE_DOWNLOAD']
+        up_limit = config_dict['QUEUE_UPLOAD']
+        async with queue_dict_lock:
+            dl = len(non_queued_dl)
+            up = len(non_queued_up)
+            all_ = dl + up
+            if all_ < all_limit:
+                f_tasks = all_limit - all_
+                if queued_up and (not up_limit or up < up_limit):
+                    for index, uid in enumerate(list(queued_up.keys()), start=1):
+                        f_tasks = all_limit - all_
+                        start_up_from_queued(uid)
+                        f_tasks -= 1
+                        if f_tasks == 0 or (up_limit and index >= up_limit - up):
+                            break
+                if queued_dl and (not dl_limit or dl < dl_limit) and f_tasks != 0:
+                    for index, uid in enumerate(list(queued_dl.keys()), start=1):
+                        start_dl_from_queued(uid)
+                        if (dl_limit and index >= dl_limit - dl) or index == f_tasks:
+                            break
+        return
+
+    if up_limit := config_dict['QUEUE_UPLOAD']:
+        async with queue_dict_lock:
+            up = len(non_queued_up)
+            if queued_up and up < up_limit:
+                f_tasks = up_limit - up
+                for index, uid in enumerate(list(queued_up.keys()), start=1):
+                    start_up_from_queued(uid)
+                    if index == f_tasks:
+                        break
+    else:
+        async with queue_dict_lock:
+            if queued_up:
+                for uid in list(queued_up.keys()):
+                    start_up_from_queued(uid)
+
+    if dl_limit := config_dict['QUEUE_DOWNLOAD']:
+        async with queue_dict_lock:
+            dl = len(non_queued_dl)
+            if queued_dl and dl < dl_limit:
+                f_tasks = dl_limit - dl
+                for index, uid in enumerate(list(queued_dl.keys()), start=1):
+                    start_dl_from_queued(uid)
+                    if index == f_tasks:
+                        break
+    else:
+        async with queue_dict_lock:
+            if queued_dl:
+                for uid in list(queued_dl.keys()):
+                    start_dl_from_queued(uid)
+
+
+async def limit_checker(size, listener, isTorrent=False, isMega=False, isDriveLink=False, isLeech=False, isYtdlp=False):
+    LOGGER.info('Checking Size Limit of link/file/folder/tasks...')
+    user_id = listener.message.from_user.id
+    if user_id == OWNER_ID or user_id in user_data and user_data[user_id].get('is_sudo'):
+        return
+    limit_exceeded = ''
+    if listener.isClone:
+        if CLONE_LIMIT := config_dict['CLONE_LIMIT']:
+            limit = CLONE_LIMIT * 1024**3
+            if size > limit:
+                limit_exceeded = f'\nClone limit is {get_readable_file_size(limit)}'
+    elif isMega:
+        if MEGA_LIMIT := config_dict['MEGA_LIMIT']:
+            limit = MEGA_LIMIT * 1024**3
+            if size > limit:
+                limit_exceeded = f'\nMega limit is {get_readable_file_size(limit)}'
+    elif isDriveLink:
+        if GDRIVE_LIMIT := config_dict['GDRIVE_LIMIT']:
+            limit = GDRIVE_LIMIT * 1024**3
+            if size > limit:
+                limit_exceeded = f'\nGoogle drive limit is {get_readable_file_size(limit)}'
+    elif isYtdlp:
+        if YTDLP_LIMIT := config_dict['YTDLP_LIMIT']:
+            limit = YTDLP_LIMIT * 1024**3
+            if size > limit:
+                limit_exceeded = f'\nYtdlp limit is {get_readable_file_size(limit)}'
+    elif isTorrent:
+        if TORRENT_LIMIT := config_dict['TORRENT_LIMIT']:
+            limit = TORRENT_LIMIT * 1024**3
+            if size > limit:
+                limit_exceeded = f'\nTorrent limit is {get_readable_file_size(limit)}'
+    elif DIRECT_LIMIT := config_dict['DIRECT_LIMIT']:
+        limit = DIRECT_LIMIT * 1024**3
+        if size > limit:
+            limit_exceeded = f'\nDirect limit is {get_readable_file_size(limit)}'
+    if not limit_exceeded:
+        if (LEECH_LIMIT := config_dict['LEECH_LIMIT']) and listener.isLeech:
+            limit = LEECH_LIMIT * 1024**3
+            if size > limit:
+                limit_exceeded = f'\nLeech limit is {get_readable_file_size(limit)}'
+        
+        if (STORAGE_THRESHOLD := config_dict['STORAGE_THRESHOLD']) and not listener.isClone:
+            arch = any([listener.compress, listener.extract])
+            limit = STORAGE_THRESHOLD * 1024**3
+            acpt = await sync_to_async(check_storage_threshold, size, limit, arch)
+            if not acpt:
+                limit_exceeded = f'\nYou must leave {get_readable_file_size(limit)} free storage.'
+    if limit_exceeded:
+        return f"{limit_exceeded}.\nYour File/Folder size is {get_readable_file_size(size)}."
+
+
+async def task_utils(message):
+    LOGGER.info('Checking Task Utilities ...')
+    msg = []
+    button = None
+    token_msg, button = checking_access(message.from_user.id, button)
+    if token_msg is not None:
+        msg.append(token_msg)
+    if ids := config_dict['FSUB_IDS']:
+        _msg, button = await forcesub(message, ids, button)
+        if _msg:
+            msg.append(_msg)
+    user_id = message.from_user.id
+    user_dict = user_data.get(user_id, {})
+    user = await user_info(message._client, message.from_user.id)
+    if config_dict['BOT_PM'] or user_dict.get('bot_pm'):
+        if user.status == user.status.LONG_AGO:
+            _msg, button = await BotPm_check(message, button)
+            if _msg:
+                msg.append(_msg)
+    if message.from_user.id == OWNER_ID:
+        LOGGER.info('Bot owner detected. Skipping task checking...')
+        return msg, button
+    if len(download_dict) >= config_dict['BOT_MAX_TASKS']:
+        msg.append(f"Bot max tasks limit exceeded.\nBot max tasks limit is {config_dict['BOT_MAX_TASKS']}.\nPlease wait for the completion of other tasks.")
+        return msg, button
+    if (maxtask := config_dict['USER_MAX_TASKS']) and await get_user_tasks(message.from_user.id, maxtask):
+        msg.append(f"User's max tasks limit is {maxtask}.\nPlease wait to complete your old tasks.")
+    return msg, button
--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python3
 from string import ascii_letters
 from random import SystemRandom
-from time import sleep
-from telegraph import Telegraph
+from asyncio import sleep
+from telegraph.aio import Telegraph
 from telegraph.exceptions import RetryAfterError
 
-from bot import LOGGER, config_dict
+from bot import LOGGER, bot_loop
 
 
 class TelegraphHelper:
@@ -14,10 +15,9 @@ class TelegraphHelper:
         self.access_token = None
         self.author_name = author_name
         self.author_url = author_url
-        self.create_account()
 
-    def create_account(self):
-        self.telegraph.create_account(
+    async def create_account(self):
+        await self.telegraph.create_account(
             short_name=self.short_name,
             author_name=self.author_name,
             author_url=self.author_url
@@ -25,61 +25,59 @@ class TelegraphHelper:
         self.access_token = self.telegraph.get_access_token()
         LOGGER.info("Creating Telegraph Account")
 
-    def create_page(self, title, content):
+    async def create_page(self, title, content):
         try:
-           return self.telegraph.create_page(
-                title = title,
-                author_name=self.author_name,
-                author_url=self.author_url,
-                html_content=content
-           )
-        except RetryAfterError as st:
-            LOGGER.warning(f'Telegraph Flood control exceeded. I will sleep for {st.retry_after} seconds.')
-            sleep(st.retry_after)
-            return self.create_page(title, content)
-
-    def edit_page(self, path, title, content):
-        try:
-            return self.telegraph.edit_page(
-                path = path,
-                title = title,
+            return await self.telegraph.create_page(
+                title=title,
                 author_name=self.author_name,
                 author_url=self.author_url,
                 html_content=content
             )
         except RetryAfterError as st:
-            LOGGER.warning(f'Telegraph Flood control exceeded. I will sleep for {st.retry_after} seconds.')
-            sleep(st.retry_after)
-        return self.edit_page(path, title, content)
+            LOGGER.warning(
+                f'Telegraph Flood control exceeded. I will sleep for {st.retry_after} seconds.')
+            await sleep(st.retry_after)
+            return await self.create_page(title, content)
 
-    def edit_telegraph(self, path, telegraph_content):
-        TITLE_NAME = config_dict['TITLE_NAME']
+    async def edit_page(self, path, title, content):
+        try:
+            return await self.telegraph.edit_page(
+                path=path,
+                title=title,
+                author_name=self.author_name,
+                author_url=self.author_url,
+                html_content=content
+            )
+        except RetryAfterError as st:
+            LOGGER.warning(
+                f'Telegraph Flood control exceeded. I will sleep for {st.retry_after} seconds.')
+            await sleep(st.retry_after)
+            return await self.edit_page(path, title, content)
+
+    async def edit_telegraph(self, path, telegraph_content):
         nxt_page = 1
         prev_page = 0
         num_of_path = len(path)
-        for content in telegraph_content :
-            if nxt_page == 1 :
+        for content in telegraph_content:
+            if nxt_page == 1:
                 content += f'<b><a href="https://telegra.ph/{path[nxt_page]}">Next</a></b>'
                 nxt_page += 1
-            else :
+            else:
                 if prev_page <= num_of_path:
                     content += f'<b><a href="https://telegra.ph/{path[prev_page]}">Prev</a></b>'
                     prev_page += 1
                 if nxt_page < num_of_path:
                     content += f'<b> | <a href="https://telegra.ph/{path[nxt_page]}">Next</a></b>'
                     nxt_page += 1
-            self.edit_page(
-                path = path[prev_page],
-                title = f"{config_dict['TITLE_NAME']} Torrent Search",
+            await self.edit_page(
+                path=path[prev_page],
+                title='Mirror-leech-bot Torrent Search',
                 content=content
             )
         return
 
-try:
-    AUTHOR_NAME = config_dict['AUTHOR_NAME']
-    AUTHOR_URL = config_dict['AUTHOR_URL']
-    telegraph=TelegraphHelper(f"{config_dict['AUTHOR_NAME']}", f"{config_dict['AUTHOR_URL']}")
-except Exception as err:
-    LOGGER.warning(f"Can't Create Telegraph Account: {err}")
-    telegraph = None
-    pass
+
+telegraph = TelegraphHelper('Atrocious-Telegram-Mirror-Bot',
+                            'https://github.com/SN-Abdullah-Al-Noman/Atrocious_Mirror')
+
+bot_loop.run_until_complete(telegraph.create_account())
