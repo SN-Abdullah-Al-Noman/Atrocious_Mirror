2c8b0a94ed1c500b42c0345e51dc7720510fe4d1
Abdullah Al Noman <101085500+Al-Noman-12@users.noreply.github.com>
2023-12-16 17:44:39 +0600

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -186,7 +186,7 @@ def get_readable_message():
         TASKS_COUNT = f"<b>Task Limit: </b>{config_dict['BOT_MAX_TASKS']} | <b>Run:</b> {tasks} | <b>Free:</b> {config_dict['BOT_MAX_TASKS'] - tasks}"
     else:
         TASKS_COUNT = f"<b>Tasks Running:</b> {tasks}"
-    msg += f"________________________________"
+    
     msg += f"\n{TASKS_COUNT}\n"
     msg += f"<b>CPU:</b> {cpu_percent()}% | <b>FREE:</b> {get_readable_file_size(disk_usage(config_dict['DOWNLOAD_DIR']).free)}"
     msg += f"\n<b>RAM:</b> {virtual_memory().percent}% | <b>UP:</b> {get_readable_time(time() - botStartTime)}"
