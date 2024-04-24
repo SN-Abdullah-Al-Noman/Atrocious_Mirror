c2aee08734018c92dba8298f6f59b4ece6d41a9d
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2024-04-23 22:19:52 -0700

    Update direct_downloader.py

--- bot/helper/mirror_utils/download_utils/direct_downloader.py
+++ bot/helper/mirror_utils/download_utils/direct_downloader.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 from secrets import token_urlsafe
 
-from bot import (LOGGER, aria2_options, aria2c_global, download_dict,
-                 download_dict_lock, non_queued_dl, queue_dict_lock)
+from bot import LOGGER, aria2_options, aria2c_global, download_dict, download_dict_lock
 from bot.helper.ext_utils.bot_utils import sync_to_async
 from bot.helper.ext_utils.task_manager import is_queued
 from bot.helper.listeners.direct_listener import DirectListener
@@ -29,22 +28,6 @@ async def add_direct_download(details, path, listener, foldername):
         return
 
     gid = token_urlsafe(10)
-    added_to_queue, event = await is_queued(listener.uid)
-    if added_to_queue:
-        LOGGER.info(f"Added to Queue/Download: {foldername}")
-        async with download_dict_lock:
-            download_dict[listener.uid] = QueueStatus(
-                foldername, size, gid, listener, 'dl')
-        await listener.onDownloadStart()
-        await sendStatusMessage(listener.message)
-        await event.wait()
-        async with download_dict_lock:
-            if listener.uid not in download_dict:
-                return
-        from_queue = True
-    else:
-        from_queue = False
-
     a2c_opt = {**aria2_options}
     [a2c_opt.pop(k) for k in aria2c_global if k in aria2_options]
     if header:= details.get('header'):
@@ -55,14 +38,4 @@ async def add_direct_download(details, path, listener, foldername):
     async with download_dict_lock:
         download_dict[listener.uid] = DirectStatus(directListener, gid, listener)
 
-    async with queue_dict_lock:
-        non_queued_dl.add(listener.uid)
-
-    if from_queue:
-        LOGGER.info(f'Start Queued Download from Direct Download: {foldername}')
-    else:
-        LOGGER.info(f"Download from Direct Download: {foldername}")
-        await listener.onDownloadStart()
-        await sendStatusMessage(listener.message)
-
-    await sync_to_async(directListener.download, contents)
+    await sync_to_async(directListener.download, contents
