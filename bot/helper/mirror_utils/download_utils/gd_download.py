7b90544285adc06d7af0966441a9a6100ae1bb7b
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2024-04-23 22:22:21 -0700

    Update gd_download.py

--- bot/helper/mirror_utils/download_utils/gd_download.py
+++ bot/helper/mirror_utils/download_utils/gd_download.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from secrets import token_urlsafe
 
-from bot import download_dict, download_dict_lock, LOGGER, non_queued_dl, queue_dict_lock
+from bot import download_dict, download_dict_lock, LOGGER
 from bot.helper.mirror_utils.gdrive_utlis.download import gdDownload
 from bot.helper.mirror_utils.gdrive_utlis.count import gdCount
 from bot.helper.mirror_utils.status_utils.gdrive_status import GdriveStatus
@@ -45,33 +45,11 @@ async def add_gd_download(link, path, listener, newname):
         await sendMessage(listener.message, msg, button)
         return
 
-    added_to_queue, event = await is_queued(listener.uid)
-    if added_to_queue:
-        LOGGER.info(f"Added to Queue/Download: {name}")
-        async with download_dict_lock:
-            download_dict[listener.uid] = QueueStatus(
-                name, size, gid, listener, 'dl')
-        await listener.onDownloadStart()
-        await sendStatusMessage(listener.message)
-        await event.wait()
-        async with download_dict_lock:
-            if listener.uid not in download_dict:
-                return
-        from_queue = True
-    else:
-        from_queue = False
-
     drive = gdDownload(name, path, listener)
     async with download_dict_lock:
         download_dict[listener.uid] = GdriveStatus(
             drive, size, listener.message, gid, 'dl')
 
-    async with queue_dict_lock:
-        non_queued_dl.add(listener.uid)
-
-    if from_queue:
-        LOGGER.info(f'Start Queued Download from GDrive: {name}')
-    else:
         LOGGER.info(f"Download from GDrive: {name}")
         await listener.onDownloadStart()
         await sendStatusMessage(listener.message)
