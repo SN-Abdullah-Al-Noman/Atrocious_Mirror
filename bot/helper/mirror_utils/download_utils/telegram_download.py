ebd5f2cbd19e85061b456c5966419c5a821d9ad5
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2024-04-23 22:36:21 -0700

    Update telegram_download.py

--- bot/helper/mirror_utils/download_utils/telegram_download.py
+++ bot/helper/mirror_utils/download_utils/telegram_download.py
@@ -6,7 +6,6 @@ from asyncio import Lock
 from bot import LOGGER, download_dict, download_dict_lock, bot, user, IS_PREMIUM_USER, config_dict
 from bot.helper.mirror_utils.status_utils.telegram_status import TelegramStatus
 from bot.helper.telegram_helper.message_utils import sendStatusMessage, sendMessage
-from bot.helper.ext_utils.task_manager import is_queued
 from bot.helper.ext_utils.atrocious_utils import stop_duplicate_check, stop_duplicate_leech
 
 
