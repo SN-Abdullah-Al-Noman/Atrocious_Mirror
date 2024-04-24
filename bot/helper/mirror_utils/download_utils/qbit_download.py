cb422fcea762fab0c428b85c59ee2afa7273df3b
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2024-04-23 22:28:24 -0700

    Update qbit_download.py

--- bot/helper/mirror_utils/download_utils/qbit_download.py
+++ bot/helper/mirror_utils/download_utils/qbit_download.py
@@ -2,7 +2,7 @@
 from time import time
 from aiofiles.os import remove as aioremove, path as aiopath
 
-from bot import download_dict, download_dict_lock, get_client, LOGGER, config_dict, non_queued_dl, queue_dict_lock
+from bot import download_dict, download_dict_lock, get_client, LOGGER, config_dict
 from bot.helper.mirror_utils.status_utils.qbit_status import QbittorrentStatus
 from bot.helper.telegram_helper.message_utils import sendMessage, deleteMessage, sendStatusMessage
 from bot.helper.ext_utils.bot_utils import bt_selection_buttons, sync_to_async
@@ -59,20 +59,7 @@ async def add_qb_torrent(link, path, listener, ratio, seed_time):
             await sendMessage(listener.message, "This Torrent already added or unsupported/invalid link/file.")
             return
 
-        async with download_dict_lock:
-            download_dict[listener.uid] = QbittorrentStatus(
-                listener, queued=added_to_queue)
         await onDownloadStart(f'{listener.uid}')
-
-        if added_to_queue:
-            LOGGER.info(
-                f"Added to Queue/Download: {tor_info.name} - Hash: {ext_hash}")
-        else:
-            async with queue_dict_lock:
-                non_queued_dl.add(listener.uid)
-            LOGGER.info(
-                f"QbitDownload started: {tor_info.name} - Hash: {ext_hash}")
-
         await listener.onDownloadStart()
 
         if config_dict['BASE_URL'] and listener.select:
@@ -94,28 +81,16 @@ async def add_qb_torrent(link, path, listener, ratio, seed_time):
                         return
 
             ext_hash = tor_info.hash
-            if not added_to_queue:
-                await sync_to_async(client.torrents_pause, torrent_hashes=ext_hash)
             SBUTTONS = bt_selection_buttons(ext_hash)
             msg = "Your download paused. Choose files then press Done Selecting button to start downloading."
             await sendMessage(listener.message, msg, SBUTTONS)
         else:
             await sendStatusMessage(listener.message)
 
-        if added_to_queue:
-            await event.wait()
-
-            async with download_dict_lock:
-                if listener.uid not in download_dict:
-                    return
-                download_dict[listener.uid].queued = False
-
             await sync_to_async(client.torrents_resume, torrent_hashes=ext_hash)
             LOGGER.info(
                 f'Start Queued Download from Qbittorrent: {tor_info.name} - Hash: {ext_hash}')
 
-            async with queue_dict_lock:
-                non_queued_dl.add(listener.uid)
     except Exception as e:
         await sendMessage(listener.message, str(e))
     finally:
