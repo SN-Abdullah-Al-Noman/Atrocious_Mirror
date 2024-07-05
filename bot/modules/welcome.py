451cfe28893aaa8a53b778fe566bf1f8a76dcbaf
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2024-07-05 22:56:47 +0600

    Update welcome.py

--- bot/modules/welcome.py
+++ bot/modules/welcome.py
@@ -8,18 +8,15 @@ from bot import bot
 async def welcome(client, message):
     for new_member in message.new_chat_members:
         member_name = new_member.first_name if new_member.first_name else "there"
-        user_name = new_member.username if new_member.username else ""
+        user_name = f"@{new_member.username}" if new_member.username else ""
         user_id = new_member.id
         now = datetime.now()
         join_date = now.strftime("%Y-%m-%d")
         join_time = now.strftime("%I:%M:%S %p")
         chat_members_count = await client.get_chat_members_count(message.chat.id)
-        welcome_message = (
-            f"Hlw {member_name} {user_name}\n"
-            f"Your ID: <code>{user_id}</code>\n"
-            f"Join date: {join_date}\n"
-            f"Join time: {join_time}\n"
-            f"Member number: {chat_members_count}"
-        )
-
-        await message.reply_text(welcome_message)
+        msg = f"Hlw {member_name} {user_name}\n"
+        msg += f"Your ID: <code>{user_id}</code>\n"
+        msg += f"Join date: {join_date}\n"
+        msg += f"Join time: {join_time}\n"
+        msg += f"Member number: {chat_members_count}"
+        await message.reply_text(msg)
